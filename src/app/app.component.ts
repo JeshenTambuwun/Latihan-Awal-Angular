6f82f85646b0fd2e4754e26e4575c7f5caf0db07
JeshenTambuwun <c14180256@john.petra.ac.id>
2020-04-30 19:46:34 +0800

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,117 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  jumlahoperator=0;
+  angkapertama="";
+  angkakedua="";
+  operator="";
+  kali(){
+    if(this.jumlahoperator==0){
+      this.operator+='*'
+      this.jumlahoperator++;
+    }
+    else{
+      this.operator='';
+      this.jumlahoperator=0;
+    }
+  }
+  kurang(){
+    if(this.jumlahoperator==0){
+      this.operator+='-'
+      this.jumlahoperator++;
+    }
+    else{
+      this.operator='';
+      this.jumlahoperator=0;
+    }
+  }
+  tambah(){
+    if(this.jumlahoperator==0){
+      this.operator+='+'
+      this.jumlahoperator++;
+    }
+    else{
+      this.operator='';
+      this.jumlahoperator=0;
+    }
+  }
+  hapus(){
+    this.operator='';
+    this.angkapertama='';
+    this.angkakedua='';
+    this.jumlahoperator=0;
+  }
+  satu(){
+    if(this.angkakedua==''&&this.operator==''){
+      this.angkapertama+='1';
+    } 
+    if(this.angkapertama!='' && this.operator!=''){
+      this.angkakedua+='1';
+    }
+  }
+  dua(){
+        if(this.angkakedua==''&&this.operator==''){
+      this.angkapertama+='2';
+    } 
+    if(this.angkapertama!='' && this.operator!=''){
+      this.angkakedua+='2';
+    }
+  }
+  tiga(){
+        if(this.angkakedua==''&&this.operator==''){
+      this.angkapertama+='3';
+    } 
+    if(this.angkapertama!='' && this.operator!=''){
+      this.angkakedua+='3';
+    }
+  }
+  empat(){
+    if(this.angkakedua==''&&this.operator==''){
+      this.angkapertama+='4';
+    } 
+    if(this.angkapertama!='' && this.operator!=''){
+      this.angkakedua+='4';
+    }
+  }
+  lima(){
+    if(this.angkakedua==''&&this.operator==''){
+      this.angkapertama+='5';
+    } 
+    if(this.angkapertama!='' && this.operator!=''){
+      this.angkakedua+='5';
+    }
+  }
+  enam(){
+    if(this.angkakedua==''&&this.operator==''){
+      this.angkapertama+='6';
+    } 
+    if(this.angkapertama!='' && this.operator!=''){
+      this.angkakedua+='6';
+    }
+  }
+  tujuh(){
+    if(this.angkakedua==''&&this.operator==''){
+      this.angkapertama+='7';
+    } 
+    if(this.angkapertama!='' && this.operator!=''){
+      this.angkakedua+='7';
+    }
+  }
+  delapan(){
+    if(this.angkakedua==''&&this.operator==''){
+      this.angkapertama+='8';
+    } 
+    if(this.angkapertama!='' && this.operator!=''){
+      this.angkakedua+='8';
+    }
+  }
+  sembilan(){
+    if(this.angkakedua==''&&this.operator==''){
+      this.angkapertama+='9';
+    } 
+    if(this.angkapertama!='' && this.operator!=''){
+      this.angkakedua+='9';
+    }
+  }
+
 }
